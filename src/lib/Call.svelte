3c688197cc413b422ce7074f8e07bb5d58f12d81
Aliervo <aliervo@wingedvengeance.net>
2022-05-11 19:14:01 -0500

    Move Call now button into a new component

---
+++ src/lib/Call.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let phone = "7855554321";
+</script>
+
+<a href="tel:{phone}">
+  <img src="phone.svg" alt="Phone icon" />
+  Call or Text:<br/>
+  {`(${phone.slice(0,3)})${phone.slice(3,6)}-${phone.slice(6)}`}
+</a>
+
+<style>
+  img {
+    height: inherit;
+  }
+
+  a {
+    display: flex;
+    position: fixed;
+    bottom: 1em;
+    right: 1em;
+    height: 2em;
+    width: 10em;
+    text-decoration: none;
+    color: black;
+    background-color: blue;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: .5em .25em;
+    border-radius: 2em;
+    box-shadow: .4em .35em .25em lightslategrey;
+ }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,7 @@
 <script>
+  import Call from "$lib/Call.svelte";
   import Nav from "$lib/Nav.svelte";
   import "../app.css";
-
-  const phone = "7855554321"
 </script>
 
 <Nav />
@@ -12,29 +11,8 @@
 </main>
 
 <footer>
-  <img src="phone.svg" alt="Phone icon" />
-  Call or Text:<br/>
-  <a href="tel:{phone}">{`(${phone.slice(0,3)})${phone.slice(3,6)}-${phone.slice(6)}`}</a>
+  <Call />
 </footer>
 
 <style>
-  footer {
-    position: fixed;
-    right: 1em;
-    bottom: 1em;
-    background-color: blue;
-    display: flex;
-    align-items: center;
-    width: 13em;
-    height: 3em;
-  }
-
-  img {
-    height: inherit;
-  }
-
-  a {
-    text-decoration: none;
-    color: black;
-  }
 </style>
