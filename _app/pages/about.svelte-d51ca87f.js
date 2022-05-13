39cb9f443e97aa417d40c58be81379d8d669e024
Aliervo <aliervo@wingedvengeance.net>
2022-05-12 22:30:38 -0500

    Initial build commit

---
+++ _app/pages/about.svelte-d51ca87f.js
@@ -0,0 +1,9 @@
+import{S as m,i as c,s as p,e as d,t as f,k as _,x as $,c as x,a as h,h as v,d as u,m as b,y as q,g as l,G as g,z as y,n as S,r as k,p as w,C as A}from"../chunks/index-edf25201.js";import{S as C}from"../chunks/SvelteMarkdown-b5967678.js";function E(s){let t,r,o,a,i;return a=new C({props:{source:s[0]}}),{c(){t=d("h1"),r=f("About"),o=_(),$(a.$$.fragment)},l(e){t=x(e,"H1",{});var n=h(t);r=v(n,"About"),n.forEach(u),o=b(e),q(a.$$.fragment,e)},m(e,n){l(e,t,n),g(t,r),l(e,o,n),y(a,e,n),i=!0},p:S,i(e){i||(k(a.$$.fragment,e),i=!0)},o(e){w(a.$$.fragment,e),i=!1},d(e){e&&u(t),e&&u(o),A(a,e)}}}function z(s){return[`
+    Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `]}class H extends m{constructor(t){super(),c(this,t,z,E,p,{})}}export{H as default};
