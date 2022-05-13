6f884c77f3847ec0baa2e9c7dfd27e06dcbdfd3c
Aliervo <aliervo@wingedvengeance.net>
2022-05-12 22:30:38 -0500

    Initial build commit

---
+++ build/_app/pages/packages.svelte-e6993668.js
@@ -0,0 +1,6 @@
+import{S as m,i as l,s as p,e as f,t as _,k as d,x as g,c as $,a as h,h as k,d as c,m as v,y as x,g as u,G as S,z as q,n as y,r as w,p as P,C}from"../chunks/index-edf25201.js";import{S as E}from"../chunks/SvelteMarkdown-b5967678.js";function j(r){let t,o,n,a,i;return a=new E({props:{source:r[0]}}),{c(){t=f("h1"),o=_("Packages"),n=d(),g(a.$$.fragment)},l(e){t=$(e,"H1",{});var s=h(t);o=k(s,"Packages"),s.forEach(c),n=v(e),x(a.$$.fragment,e)},m(e,s){u(e,t,s),S(t,o),u(e,n,s),q(a,e,s),i=!0},p:y,i(e){i||(w(a.$$.fragment,e),i=!0)},o(e){P(a.$$.fragment,e),i=!1},d(e){e&&c(t),e&&c(n),C(a,e)}}}function z(r){return[`
+    * In luctus augue ut facilisis pretium.
+    * Aliquam quis purus ac nunc vehicula sagittis.
+    * Sed ac quam porta, venenatis metus eget, imperdiet diam.
+    * Etiam a est lacinia justo sagittis congue.
+    `]}class H extends m{constructor(t){super(),l(this,t,z,j,p,{})}}export{H as default};
