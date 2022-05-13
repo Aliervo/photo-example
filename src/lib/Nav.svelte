4a8cd35a7ec57dcb73e18d21523cc09232f79861
Aliervo <aliervo@wingedvengeance.net>
2022-05-13 10:56:47 -0500

    Add base path to Nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,10 +1,14 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
 <nav>
   <svg viewBox="0 0 2 3" aria-hidden="true">
     <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
   </svg>
-  <a href="/about">About</a>
-  <a href="/">Gallery</a>
-  <a href="/packages">Packages</a>
+  <a href="{base}/about">About</a>
+  <a href="{base}/">Gallery</a>
+  <a href="{base}/packages">Packages</a>
   <svg viewBox="0 0 2 3" aria-hidden="true">
     <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
   </svg>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,10 @@
   import Call from "$lib/Call.svelte";
   import Nav from "$lib/Nav.svelte";
   import "../app.css";
+
+  import { assets } from "$app/paths";
+
+  console.log(assets);
 </script>
 
 <Nav />
